f34baa4fa9a464816e13b6702fdf917d0d6e2f90
hzae <eyanstagaan@gmail.com>
2023-01-10 19:36:46 +0800

    new admin account record

--- admin.php
+++ admin.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LH-PH: Admin</title>
 
-     <!--------- Stylesheet ------------>
-     <link rel="stylesheet" href="./assets/css/style.css"></link>
-     <!--------- Icon ------------>
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-     <!--------- Script ------------>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" ></script>
-     <script src="./assets/js/ajax.js"></script>
-     <script src="./assets/js/script.js"></script>
-     <script src="/script.js"></script>
-     <script src="data.js"></script>
-	<script src="index.js"></script>
+    <!--------- Stylesheet ------------>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    </link>
+    <!--------- Icon ------------>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <!--------- Script ------------>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
 
-    
+
 </head>
+
 <body>
     <?php
-        include "./adminViews/adminHeader.php";
-        include "./adminViews/sidebar.php";
-        
+    include "adminViews/adminHeader.php";
+    include "adminViews/sidebar.php";
+
     ?>
 
-   <div class="container allContent" id="main-content">
+    <div class=" allContent" id="main-content">
         <div class="dashboard">
             <div class="dashboard-row">
                 <div class="admin-overview">
-                <i class="fa-solid fa-users-line"></i>
+                    <i class="fa-solid fa-users-line"></i>
                     <div class="overview-desc">
                         <h1>48</h1>
                         <h4>Residents</h4>
@@ -41,7 +39,7 @@
             </div>
             <div class="dashboard-row">
                 <div class="admin-overview">
-                <i class="fa-solid fa-people-roof"></i>
+                    <i class="fa-solid fa-people-roof"></i>
                     <div class="overview-desc">
                         <h1>1000</h1>
                         <h4>Homeowners</h4>
@@ -50,9 +48,9 @@
             </div>
         </div>
         <div class="dashboard">
-              <div class="dashboard-row">
+            <div class="dashboard-row">
                 <div class="admin-overview">
-                <i class="fa-solid fa-file-circle-xmark"></i>
+                    <i class="fa-solid fa-file-circle-xmark"></i>
                     <div class="overview-desc">
                         <h1>500</h1>
                         <h4>Delinquent</h4>
@@ -61,7 +59,7 @@
             </div>
             <div class="dashboard-row">
                 <div class="admin-overview">
-                <i class="fa-solid fa-file-circle-check"></i>
+                    <i class="fa-solid fa-file-circle-check"></i>
                     <div class="overview-desc">
                         <h1>234</h1>
                         <h4>Updated</h4>
@@ -69,8 +67,16 @@
                 </div>
             </div>
         </div>
-   </div>
-   
+    </div>
+
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="assets/js/ajax.js"></script>
+<script src="assets/js/script.js"></script>
+<script src="script.js"></script>
+<script src="data.js"></script>
+<script src="index.js"></script>
+
 </html>
--- adminViews/adminHeader.php
+++ adminViews/adminHeader.php
@@ -1,21 +1,21 @@
 <div class="admin-header">
     <nav class="admin-head">
         <div class="date-timezone">
-            <h3>
+            <span>
                 <?php
                 date_default_timezone_set("Asia/Manila");
                 echo " ".date("D");
                 echo " | ".date("h:i A");
                 ?>
-            </h3>
+            </span>
         </div>
         <div class="admin-profile">
             <div class="admin-pic">
                 <img src="assets/images/user-icon.jpg">
             </div>
             <div class="admin-desc">
-                <h3>Ahanz Carl Fernandez</h3>
-                <h3>ADMIN</h3>
+                <span>Ahanz Carl Fernandez</span><br>
+                <span>ADMIN</span>
             </div>
         </div>
     </nav>
