8a91160e2398efde0c882ff0d58129c16b430df5
Reyner <jreyner123@gmail.com>
2023-01-13 06:22:04 +0800

    natanggal ko yung error sa line 12 pero di naman nalabas yung info hahaha

--- adminViews/includes/mapdata.php
+++ adminViews/includes/mapdata.php
@@ -2,9 +2,7 @@
 
 include ('connection.php');
 
-extract($_POST);
-if(isset($_POST['blockSend']) && isset($_POST['lotSend']) && isset($_POST['streetSend']) && isset($_POST['statusSend']) 
-&& isset($_POST['areaSend']) && isset($_POST['priceSend']) && isset($_POST['remarksSend']))
+
 
     $sql= "UPDATE `lot_information` SET `block`='[]',`lot`='[]',`street`='[]',
     `status`='[]',`area`='[]',`price`='[]',`remarks`='[]' WHERE 1";
