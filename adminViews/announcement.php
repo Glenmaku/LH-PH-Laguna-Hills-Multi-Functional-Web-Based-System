61b343912a86ad99d2774edd97b4e2ff7bab9a6a
hzae <eyanstagaan@gmail.com>
2023-01-13 14:48:45 +0800

    announcement

--- adminViews/announcement.php
+++ adminViews/announcement.php
@@ -23,50 +23,32 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="mb-3">
-                        <label for="announcementTitle" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="announcementTitle" placeholder=" Enter your Title" required>
-                    </div>
-                    <div class="form-floating">
-                        <textarea class="form-control" id="announcementDescription" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                        <label for="floatingTextarea2">Description</label><br>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="file" class="form-control" id="inputGroupFile02">
-                        <label class="input-group-text" for="inputGroupFile02">Upload</label>
-                    </div>
+                    <form id="announcement-form" enctype='multipart/form-data'>
+                        <div class="mb-3">
+                            <label for="announcementTitle" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="announcementTitle" placeholder=" Enter your Title" required>
+                        </div>
+                        <div class="form-floating">
+                            <textarea class="form-control" id="announcementDescription" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                            <label for="floatingTextarea2">Description</label><br>
+                        </div>
+                        <div class="input-group mb-3">
+
+                            <input type="file" class="form-control" id="attachmentFile">
+                            <input type="hidden" name="hidden_upload_folder" id="hidden_upload_folder">
+
+                        </div>
+                    </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" id="cancel-btn" data-bs-dismiss="modal">Cancel</button>
-                    <button type="button" id="submit-btn" onclick="">Submit</button>
+                    <button type="button" class="announcement-submit-btn" id="announcement-submit-btn" onclick="">Submit</button>
                 </div>
             </div>
         </div>
     </div>
 
     <script>
-        function addAnnouncement() {
-            var fname = $('#announcementTitle').val();
-            var lname = $('#announcementDescription').val();
-            var email = $('#email').val();
-            var user = $('#username').val();
-            var pass = $('#pass').val();
+       
 
-            $.ajax({
-                url: "adminViews/includes/Act-AddAdmin.php",
-                type: 'post',
-                data: {
-                    fnameSend: fname,
-                    lnameSend: lname,
-                    emailSend: email,
-                    userSend: user,
-                    passSend: pass
-                },
-                success: function(data, status) {
-                    $('#addAdminAccount').modal("hide");
-                    displayData();
-                }
-
-            });
-        }
     </script>
