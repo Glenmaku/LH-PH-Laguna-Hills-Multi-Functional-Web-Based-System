1ed19ac4ea993c2b623f45ffe11e747f0c85cf63
Reyner <jreyner123@gmail.com>
2023-01-28 23:19:30 +0800

    hover palang

--- PropertyFinder.php
+++ PropertyFinder.php
@@ -17,5 +17,30 @@
 
 </section>
 
+<script>
+
+var paths = document.querySelectorAll('.mapping');
+  paths.forEach(function(path) {
+    path.addEventListener('mouseover', function() {
+      this.style.fill = "#085D40";
+    });
+    path.addEventListener('mouseout', function() {
+      this.style.fill = "grey";
+    });
+  });
+
+  function availData(elementId) {
+    const id = elementId;
+    fetch(`adminViews/includes/mapsubmit.php?id=${id}&status=available`)
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("map-data").innerHTML = data;
+      });
+  }
+
+
+
+</script>
+
 
 <?php require_once('footer.php'); ?>
