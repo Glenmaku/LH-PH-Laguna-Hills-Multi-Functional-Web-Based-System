b0890dd29dcfec3c899516f0b72f473a41700076
Reyner <jreyner123@gmail.com>
2023-01-28 13:06:22 +0800

    Revert "Revert "Merge branch 'Hzae' of https://github.com/glenmark12-me/LHPH-System--- into Hzae""
    This reverts commit 1e3284143fc63854f4ce393af6395841ccd323c3.

--- homeownerViews/homeownerHeader.php
+++ homeownerViews/homeownerHeader.php
@@ -11,13 +11,13 @@
                         <img src="assets/images/user-icon.jpg">
                     </div>
                     <div class="owner-desc">
-                        <span>Glen Mark Dela Cruz</span>
+                        <span><?php echo $Fname." ".$Lname?></span>
                         <span>RESIDENT</span>
                     </div>
                 </div>
             </a>
             <ul class="dropdown-menu dropdown-menu-end">
-                <li><a class="dropdown-item" href="#"><i class="fa-regular fa-circle-user"></i> My Account</a></li>
+                <li><a class="dropdown-item" onclick="change_pass()" id="btn_change-pass"><i class="fa-regular fa-circle-user"></i> My Account</a></li>
                 <li><a class="dropdown-item" onclick="get_user_info()" id="btn_setting_owner_acc" class="btn_setting_owner_acc"><i class="fa-solid fa-fingerprint"></i> Owner Information</a></li>
                 <li><a class="dropdown-item" href="index.php"><i class="fa-solid fa-right-from-bracket"></i> Logout</a></li>
             </ul>
@@ -39,8 +39,8 @@
             <div class="modal-body">
                 <div id="message-updateownerinfo"></div>
                 <form class="row g-3">
-                    <input type="hidden" name="owner_username" id="owner_username" value="<?php echo $username ?>" readonly>
-                    <input type="hidden" name="ownerview_id" id="ownerview_id" value="<?php echo $ownerid ?>" readonly>
+                    <input  type="hidden" name="owner_username" id="owner_username" value="<?php echo $username ?>" readonly>
+                    <input  type="hidden" name="ownerview_id" id="ownerview_id" value="<?php echo $ownerid ?>" readonly>
                     <div class="col-12">
                         <label for="name" class="form-label">First Name</label>
                         <input type="text" class="form-control" name="owner_fname" id="owner_fname">
@@ -91,39 +91,49 @@
     </div>
 </div>
 
+<div class="modal fade" id="ResetPasswordModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        </button>
+                    </div>
+                    <form class="reset-pass-form">
+                    <div class="modal-body m-3">
+                        <h3 class="text-center">Change Password</h3>
 
-<div class="modal" tabindex="-1">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Change Password</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="col-12">
-                    <label for="change-pass" class="form-label">Enter old password</label>
-                    <input type="password" name="owner_pass" id="owner_pass" class="form-control" placeholder="">
+                        <div class="alert alert-success alert-dismissible fade show " role="alert" id="reset-error">
+                            Please enter your password to enable password update.
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+
+                        
+
+                            <!--PALATANDAAN--> 
+                            <input  name="owner_username" id="owner_username" value="<?php echo $username ?>" readonly hidden >
+                            <input name="ownerview_id" id="ownerview_id" value="<?php echo $ownerid?>" readonly hidden> <!--PALATANDAAN--> 
+
+                            <div class="form-group">
+                                <label for="" class="fs-6 mb-2"><b>Old Password</b></label>
+                                <input type="password" name="old_pass" id="old_pass" class="form-control mb-2" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for=""class="fs-6 mb-2"><b>Enter Password</b></label>
+                                <input type="password" name="new_pass" id="new_pass" class="form-control mb-2" placeholder="" disabled>
+                            </div>
+                            <div class="form-group" class="fs-6 mb-2" >
+                                <label for="owner_email"><b>Confirm Password</b></label>
+                                <input type="password" name="conf_pass" id="conf_pass" class="form-control mb-2" placeholder="" disabled>
+                            </div>
+                        </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btn_close">Cancel</button>
+                        <button type="button" class="btn btn-primary" id="btn_check_pass" name="btn_updateuserinfo" >Verify</button>
+                    </div>
+                    </form>
                 </div>
-                <div class="col-12">
-                    <label for="change-pass" class="form-label">Enter new password</label>
-                    <input type="password" name="new_owner_pass" id="new_owner_pass" class="form-control" placeholder="">
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-success">Save changes</button>
             </div>
         </div>
-    </div>
-</div>
-
-
-
-
-
-
-
-
 
 <script>
     //SCRIPTS FOR UPDATING ACCOUNT BUTTONS
@@ -185,11 +195,7 @@
     }
 
 
-
-
-
 // clock
-
 setInterval(function() {
     var currentTime = new Date();
     var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -213,5 +219,89 @@ setInterval(function() {
     $("#date-timezone").text(clock);
   }, 1000);
 
+function change_pass(){
+    $('#btn_check_pass').on('click',function(){
+        var old_pass = $("#old_pass").val();
+        var owner_username = $("#owner_username").val();
+        $.ajax({
+                    type: "POST",
+                    url: "homeownerViews/includes/act-check-password.php",
+                    data: { old_pass: old_pass, owner_username:owner_username},
+                    success: function(response) {
+                        if (response === "success") {
+                            document.getElementById("new_pass").disabled = false;
+                            document.getElementById("conf_pass").disabled = false;
+                            document.getElementById("btn_check_pass").outerHTML = '<button type="button" class="btn btn-primary" id="btn_update_pass" name="btn_updateuserpass" onclick="update_pass()">Update Password</button>';
+                            document.getElementById("reset-error").innerHTML = "You can now enter your new password.";
+                            document.getElementById("reset-error").classList.remove("alert-danger");
+                             document.getElementById("reset-error").classList.add("alert-success");
+
+                            }else if (response === "Please enter your password.") {
+                            document.getElementById("reset-error").innerHTML = "Please enter your password.";
+                            document.getElementById("reset-error").classList.add("alert-danger");
+                            document.getElementById("reset-error").classList.remove("alert-success");
+             
+
+                            } else {
+                            document.getElementById("reset-error").innerHTML = "Incorrect password. Please try again.";
+                            document.getElementById("reset-error").classList.add("alert-danger");
+                            document.getElementById("reset-error").classList.remove("alert-success");
+}
+                    }
+                })
+    });
+    $('#ResetPasswordModal').modal("show");
+}
+function update_pass(){
+  
+        var new_pass = $("#new_pass").val();
+        var conf_pass = $("#conf_pass").val();
+        var owner_username = $("#owner_username").val();
+        $.ajax({
+    type: "POST",
+    url: "homeownerViews/includes/act-update-password.php",
+    data: { new_pass: new_pass, conf_pass: conf_pass, owner_username:owner_username},
+    success:function(data){
+   
+        if (data === "success") {
+            document.getElementById("reset-error").innerHTML = "You successfully updated your password.";
+            document.getElementById("reset-error").classList.remove("alert-danger");
+            document.getElementById("reset-error").classList.add("alert-success");
+
+            $("#new_pass").val("");
+            $("#old_pass").val("");
+            $("#conf_pass").val("");
+        }else if (data === "Both fields are required. Please try again.") {
+            document.getElementById("reset-error").innerHTML = "Both fields are required. Please try again.";
+            document.getElementById("reset-error").classList.add("alert-danger");
+            document.getElementById("reset-error").classList.remove("alert-success");
+            $("#new_pass").val("");
+            $("#conf_pass").val("");
+
+        }else if (data === "Passwords do not match. Please try again.") {
+            document.getElementById("reset-error").innerHTML = "Passwords do not match. Please try again.";
+            document.getElementById("reset-error").classList.add("alert-danger");
+            document.getElementById("reset-error").classList.remove("alert-success");
+            $("#new_pass").val("");
+            $("#conf_pass").val("");
+
+        }else if (data === "The password must be at least 8 characters and contain at least 2 numbers. Please try again.") {
+            document.getElementById("reset-error").innerHTML = "The password must be at least 8 characters and contain at least 2 numbers. Please try again.";
+            document.getElementById("reset-error").classList.add("alert-danger");
+            document.getElementById("reset-error").classList.remove("alert-success");
+            $("#new_pass").val("");
+            $("#conf_pass").val("");
+
+        }else{
+            document.getElementById("reset-error").innerHTML = "An error occured. Please try again.";
+            document.getElementById("reset-error").classList.add("alert-danger");
+            document.getElementById("reset-error").classList.remove("alert-success");
+            $("#new_pass").val("");
+            $("#conf_pass").val("");
+
+        }
+    }
+})
+    }
 
 </script>
