84f9dde8ef9a47c9ece9d9c1e9b7a7f213cbd1db
Reyner <jreyner123@gmail.com>
2023-01-29 22:09:10 +0800

    Update PropertyFinder.php

--- PropertyFinder.php
+++ PropertyFinder.php
@@ -60,122 +60,18 @@
 
 
 <script>
-  $(document).ready(function() {
-    changeColor();
-    getData();
-    displayMapData();
 
+var paths = document.querySelectorAll('.mapping');
+  paths.forEach(function(path) {
+    path.addEventListener('mouseover', function() {
+      this.style.fill = "#085D40";
+    });
+    path.addEventListener('mouseout', function() {
+      this.style.fill = "grey";
+    });
   });
 
-  function displayMapData() {
 
-    var mapData = "true";
-    $.ajax({
-      url: 'adminViews/includes/mapData.php',
-      type: 'post',
-      data: {
-        mapDataSend: mapData
-      },
-      success: function(data, status) {
-        $('#finder-panel').html(data);
-
-      }
-    });
-  }
-  function getData(id) {
-			// Use fetch API for HTTP requests
-	
-			fetch(`adminViews/includes/mapsubmit.php?id=${id}`)
-
-				.then(response => response.json())
-				.then(data => {
-				
-					// Update the values of the elements with the corresponding ids
-					//document.getElementById("input-field-id").value = myId;
-					document.getElementById('Lot_ID').value = data.Lot_ID;
-					document.getElementById("block").value = data.Block;
-					document.getElementById("lot").value = data.Lot;
-					document.getElementById("street").value = data.Street;
-					document.getElementById("status").value = data.Status;
-					document.getElementById("area-per-sqm").value = data.Area;
-					document.getElementById("price").value = data.Price;
-					document.getElementById("remarkss").value = data.Remarks;
-					document.getElementById("remarks").value = data.DueRemarks;
-					document.getElementById("Monthly_Dues").value = data.Monthly_Dues;
-					document.getElementById("Yearly_Dues").value = data.Yearly_Dues;
-					document.getElementById("Dues_Status").value = data.Dues_Status;
-					document.getElementById("lotedit-id").value = data.Lot_ID;
-				})
-				.catch(error => {
-					alert(`Error: ${error}`);
-				});
-		}
-    function colorData(id) {
-			fetch('adminViews/includes/mapsubmit.php?id=' + id)
-				.then(response => response.json())
-				.then(data => {
-					if (buttonSelected === "lot-select") {
-						if (data.Status === 'available') {
-							document.getElementById(id).style.fill = '#1FCE6D';
-						} else if (data.Status === 'occupied') {
-							document.getElementById(id).style.fill = '#E94B35';
-						} else if (data.Status === 'Property Undisclosed') {
-							document.getElementById(id).style.fill = '#F2C500';
-						} else if (data.Status === 'Open Space') {
-							document.getElementById(id).style.fill = '#33495F';
-						} else if (data.Status === 'amenities') {
-							document.getElementById(id).style.fill = '#9C56B8';
-						} else if (data.Status === 'foreclosed') {
-							document.getElementById(id).style.fill = '#E87E04';
-						} else if (data.Status === 'With House') {
-							document.getElementById(id).style.fill = '#2C97DE';
-						}
-					} else if (buttonSelected === "assoc-select") {
-
-						if (data.Dues_Status === 'outdated') {
-							document.getElementById(id).style.fill = '#E12323';
-						} else if (data.Dues_Status === 'updated') {
-							document.getElementById(id).style.fill = '#018E5A';
-						} else if (data.Dues_Status === 'advanced') {
-							document.getElementById(id).style.fill = '#FDC50C';
-						} else if (data.Dues_Status === 'N/A') {
-							document.getElementById(id).style.fill = '';
-						}
-					}
-				})
-				.catch(error => {
-					console.log('Error:' + error);
-				});
-		}
-
-		const select = document.querySelector('.form-select');
-		const paths = document.querySelectorAll('path');
-
-		select.addEventListener("change", function() {
-			if (select.value === "lot-information") {
-				buttonSelected = "lot-select";
-				paths.forEach(path => {
-					getData(path.id);
-					colorData(path.id);
-				});
-			} else if (select.value === "association-dues") {
-				buttonSelected = "assoc-select";
-				paths.forEach(path => {
-					getData(path.id);
-					colorData(path.id);
-				});
-			}
-		});
-
-
-  function availData(elementId) {
-    const id = elementId;
-    fetch(`adminViews/includes/mapsubmit.php?id=${id}&status=available`)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById("map-pfinder").innerHTML = data;
-      });
-  }
 
 
   var zoomInCounter = 0;
