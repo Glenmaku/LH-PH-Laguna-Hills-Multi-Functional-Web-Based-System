be4794eb1e9f812d17fb336a01ab751e445d10a3
vennicecosino <122093873+vennicecosino@users.noreply.github.com>
2023-02-05 07:26:40 +0800

    4
    2

--- adminViews/includes/Act-AssignLot.php
+++ adminViews/includes/Act-AssignLot.php
@@ -1,4 +1,24 @@
 <?php
+// require_once('connection.php'); 
+// //require_once('functions.php'); 
+// //AssignLotsOwners();
+//         $LUsername = $_POST['LotUsername'];  
+//         $LProperty = $_POST['LotProperty'];
+//         $LOwnership = $_POST['OwnerType'];
+       
+//         if(empty($LProperty)) {
+//             echo 'Please select a property';}
+//             else{
+                
+//         $query = "insert into assigned_lot (lot_id,owner_username,ownership) 
+//         values ('$LProperty','$LUsername','$LOwnership')";
+        
+//         $result = mysqli_query($con,$query);
+//         if($result){
+//             echo'Successfully assigned property';
+//         }}
+?>
+<?php
 require_once('connection.php'); 
 //require_once('functions.php'); 
 //AssignLotsOwners();
@@ -9,12 +29,16 @@ require_once('connection.php');
         if(empty($LProperty)) {
             echo 'Please select a property';}
             else{
-                
+                $check_query = "SELECT * FROM assigned_lot WHERE lot_id = '$LProperty' AND owner_username = '$LUsername'";
+                $check_result = mysqli_query($con, $check_query);
+                if(mysqli_num_rows($check_result) > 0){
+                    echo 'This property is already assigned to this user';
+                } else {
         $query = "insert into assigned_lot (lot_id,owner_username,ownership) 
         values ('$LProperty','$LUsername','$LOwnership')";
         
         $result = mysqli_query($con,$query);
         if($result){
             echo'Successfully assigned property';
-        }}
+        }}}
 ?>
