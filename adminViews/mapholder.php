6102d48b81bfea6fa1066a79cb64ad7842f37fca
hzae <eyanstagaan@gmail.com>
2023-02-07 09:45:21 +0800

    map

--- adminViews/mapholder.php
+++ adminViews/mapholder.php
@@ -1,5 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+
 <div class="holder">
 			<iframe  class="mapFrame" id="svgMap" src="map.php" type="image/svg+xml"
 				seamless="seamless" scrolling="yes" frameborder="0"></iframe>
