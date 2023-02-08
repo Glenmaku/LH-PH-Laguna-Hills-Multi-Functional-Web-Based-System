dd62cbe694ea1355fb53d42beabed514d8510960
vennicecosino <122093873+vennicecosino@users.noreply.github.com>
2023-02-09 06:34:36 +0800

    3

--- adminViews/allTransaction.php
+++ adminViews/allTransaction.php
@@ -594,10 +594,22 @@
                 $("#assoc-reset").click(function() {
                  //   $("#trans-no").val("");
                  //   $("#date").val("");
-                    $("#client-name").val("");
-                    $("#property").val("");
-                    $("#balance-total").val("0");
-                    $("#total-balance").val("0");
+                 $("#client-name").val("");
+    $("#property").val("");
+    $("#total-balance").val("0");
+    $("#selected-balance").val("0");
+    $("#a-interest").val("0");
+    $("#a-discount").val("0");
+    $("#balance-total").val("0");
+    $("#all-total").val("0");
+    $("#a-payment").val("0");
+    $("#a-change").val("0");
+    $("#ifadvanced").prop("checked", false);
+    $("#a-remaining-balance").val("");
+    $("#a-remarks").val("");
+    $("#selected-balance").val("0");
+    $("#a-interest").val("0");
+    $("#a-discount").val("0");
 
                 });
 
