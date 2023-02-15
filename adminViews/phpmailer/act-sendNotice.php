305cc70f653170662eaa2bb5b0259d95c8aded7a
hzae <eyanstagaan@gmail.com>
2023-02-15 23:11:39 +0800

    push

--- adminViews/phpmailer/act-sendNotice.php
+++ adminViews/phpmailer/act-sendNotice.php
@@ -1,51 +1,60 @@
+
 <?php
-include 'connection.php';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+
 require_once 'PHPMailerAutoload.php';
 require_once 'class.smtp.php';
 require_once 'class.phpmailer.php';
 
-$lot_id = $_POST['Lot_ID'];
+// function dbConnection() {
+//     $conn = mysqli_connect('localhost', 'root', '', 'lhph');
 
-$query = "SELECT * FROM assigned_lot al
-  JOIN association_dues ad ON al.lot_id = ad.Lot_ID
-  JOIN owner_accounts oa ON al.owner_username = oa.owner_username
-  WHERE al.lot_id = $lot_id";
+//     if (mysqli_errno($conn) > 0) {
+//         die('hey! you just killed the connection to the database');
+//     }
+//     return $conn;
+// }
 
-$result = mysqli_query($con, $query);
-$email = mysqli_fetch_assoc($result)['owner_email'];
+function sendEmail($email, $subject, $message){
 
-while ($row = mysqli_fetch_assoc($result)) {
-    $email = $row['owner_email'];
-    $name = $row['owner_fname'] . ' ' . $row['owner_lname'];
-    $balance = $row['Balance'];
 
-    // send email to each owner
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
     $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'guyrx90@gmail.com'; //your gmail username
-    $mail->Password = 'qhufcunapjtxzeop'; //your gmail password
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-
-    $mail->setFrom('yourgmail@gmail.com', 'Laguna Hills Homeowner Association');
-    $mail->addAddress($email, $name);
-
+    $mail->isSMTP();   //Send using SMTP
+    $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;       //Enable SMTP authentication
+    $mail->Username   = 'guyrx90@gmail.com';  //SMTP username
+    $mail->Password   = 'qhufcunapjtxzeop';    //SMTP password
+    $mail->SMTPSecure = 'tls';    //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port = 587;     
     $mail->isHTML(true);
+    //Recipients
+    $mail->setFrom('emailAddress@gmail.com', 'LHPH Emailer');
 
-    $mail->Subject = 'Notice for Remaining Balance';
-    $mail->Body = "Dear $name,<br><br>
-    We hope this message finds you well. We are writing to remind you that there is currently a remaining balance on your account in the amount of $balance. As a valued customer, we kindly request that you settle this amount as soon as possible.<br><br>
-    Please note that failure to pay this balance may result in additional fees or the suspension of your account until the balance is cleared. We want to avoid any inconvenience or interruption in our service, so please do not hesitate to contact us if you have any questions or concerns.<br><br>
-    Thank you for your attention to this matter. We appreciate your business and look forward to continuing to serve you.<br><br>
-    Sincerely,<br>
-    Laguna Hills Homeowner Association";
+    $mail->addAddress($email,);     //Add a recipient
+    // $mail->addAddress('ellen@example.com');  //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
-    if ($mail->send()) {
-        echo 'Email sent successfully!';
-    } else {
-        echo 'An error occurred while sending the email.';
-    }
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');  //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg'); //Optional name
+
+    //Content   //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
+}
+
+
